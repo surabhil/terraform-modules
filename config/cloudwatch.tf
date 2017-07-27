0372646f07ac25b60aff13354ca800aaec98ec33
Paul Roever <paul@roever-family.net>
2017-07-26 18:25:54 -0700

    added config module for reusable configurations

---
+++ config/cloudwatch.tf
@@ -0,0 +1,49 @@
+resource "aws_api_gateway_account" "apigatewayaccount" {
+  cloudwatch_role_arn = "${aws_iam_role.apigateway_cloudwatch_role.arn}"
+}
+
+resource "aws_iam_role" "apigateway_cloudwatch_role" {
+  name = "api_gateway_cloudwatch_global"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "apigateway_cloudwatch_policy" {
+  name = "default"
+  role = "${aws_iam_role.apigateway_cloudwatch_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:FilterLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
