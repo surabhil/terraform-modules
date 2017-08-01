ff8807bf63b401e6bd548f27ef815e916d2d9856
Paul Roever <paul@roever-family.net>
2017-08-01 15:23:25 -0700

    added api id and stage name to outputs for both modules, this is needed for custom domain names

--- protectedresource/outputs.tf
+++ protectedresource/outputs.tf
@@ -1,3 +1,11 @@
 output "endpoint_invoke_url" {
   value = "${aws_api_gateway_deployment.protectedresource_prod.invoke_url}"
 }
+
+output "api_id" {
+  value = "${aws_api_gateway_rest_api.protectedresource.id}"
+}
+
+output "api_stage" {
+  value = "${aws_api_gateway_deployment.protectedresource_prod.stage_name}"
+}
--- unprotectedresource/outputs.tf
+++ unprotectedresource/outputs.tf
@@ -1,3 +1,11 @@
 output "endpoint_invoke_url" {
   value = "${aws_api_gateway_deployment.unprotectedresource_prod.invoke_url}"
 }
+
+output "api_id" {
+  value = "${aws_api_gateway_rest_api.unprotectedresource.id}"
+}
+
+output "api_stage" {
+  value = "${aws_api_gateway_deployment.unprotectedresource_prod.stage_name}"
+}
