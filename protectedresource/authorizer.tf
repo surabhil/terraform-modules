7edb70bff24e04760c16acfc1e033e197c96240f
Paul Roever <paul@roever-family.net>
2017-08-03 16:42:59 -0700

    stopped looking for auth function name in s3, this was redundant because finding the name required knowing the name

--- protectedresource/authorizer.tf
+++ protectedresource/authorizer.tf
@@ -1,8 +1,3 @@
-data "aws_s3_bucket_object" "auth_fn_name" {
-  bucket = "${var.config_bucket}"
-  key = "${var.authorizer_name}/function_name"
-}
-
 data "aws_s3_bucket_object" "auth_fn_arn" {
   bucket = "${var.config_bucket}"
   key = "${var.authorizer_name}/arn"
@@ -14,7 +9,7 @@ data "aws_s3_bucket_object" "auth_fn_invoke_arn" {
 }
 
 resource "aws_api_gateway_authorizer" "authorizer" {
-  name                   = "${data.aws_s3_bucket_object.auth_fn_name.body}"
+  name                   = "${var.authorizer_name}"
   rest_api_id            = "${aws_api_gateway_rest_api.protectedresource.id}"
   authorizer_uri         = "${data.aws_s3_bucket_object.auth_fn_invoke_arn.body}"
   authorizer_result_ttl_in_seconds = "0"
