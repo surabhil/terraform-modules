ca78867595fd0aa9e5c1acb78e8170597420c85f
Paul Roever <paul@roever-family.net>
2017-08-15 17:11:03 -0700

    removed module and converted everything back to resources; modules don't support counts

--- protectedresource/api-gateway.tf
+++ protectedresource/api-gateway.tf
@@ -3,26 +3,54 @@ resource "aws_api_gateway_rest_api" "protectedresource" {
   name = "${var.api_name}"
 }
 
-module "http_method" {
+resource "aws_api_gateway_resource" "rest_resources" {
   count = "${length(var.names)}"
 
-  source = "../httpresource"
+  parent_id = "${element(var.parent_ids, count.index) == "" ? aws_api_gateway_rest_api.protectedresource.root_resource_id : element(var.parent_ids, count.index)}"
+  path_part = "${element(var.paths, count.index)}"
+  rest_api_id = "${aws_api_gateway_rest_api.protectedresource.id}"
+}
 
-  aws_region = "${var.aws_region}"
+resource "aws_api_gateway_method" "http_methods" {
+  count = "${length(var.names)}"
+
+  rest_api_id   = "${aws_api_gateway_rest_api.protectedresource.id}"
+  resource_id   = "${element(aws_api_gateway_resource.rest_resources.id, count.index)}"
+  http_method   = "${element(var.names, count.index) == "" ? "ANY" : element(var.names, count.index)}"
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.authorizer.id}"
+}
+
+# add a Lambda integration, using the Lambda created in lambdas.tf
+resource "aws_api_gateway_integration" "protectedresource_integrations" {
+  count = "${length(var.names)}"
+
+  rest_api_id             = "${aws_api_gateway_rest_api.protectedresource.id}"
+  resource_id             = "${element(aws_api_gateway_resource.rest_resources.id, count.index)}"
+  http_method             = "${element(aws_api_gateway_method.http_methods.http_method, count.index)}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${element(aws_lambda_function.protectedresources.arn, count.index)}/invocations"
+}
+
+# create the corresponding response and integration response (if someone could explain this to me that would be great)
+resource "aws_api_gateway_method_response" "protectedresource_responses" {
+  count = "${length(var.names)}"
+
+  depends_on  = ["aws_api_gateway_integration.protectedresource_integration"]
+  rest_api_id = "${aws_api_gateway_rest_api.protectedresource.id}"
+  resource_id = "${element(aws_api_gateway_method.http_methods.resource_id, count.index)}"
+  http_method = "${element(aws_api_gateway_method.http_methods.http_method, count.index)}"
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "protectedresource_integrationresponses" {
+  count = "${length(var.names)}"
 
   rest_api_id = "${aws_api_gateway_rest_api.protectedresource.id}"
-
-  rest_api_root_resource_id = "${aws_api_gateway_rest_api.protectedresource.root_resource_id}"
-
-  authorizer_id = "${aws_api_gateway_authorizer.authorizer.id}"
-
-  resource_lambda_arn = "${element(aws_lambda_function.protectedresources.arn, count.index)}"
-
-  resource_path = "${element(var.paths, count.index)}"
-
-  resource_method = "${element(var.methods, count.index)}"
-
-  resource_parent_id = "${element(var.parent_ids, count.index)}"
+  resource_id = "${element(aws_api_gateway_method.http_methods.resource_id, count.index)}"
+  http_method = "${element(aws_api_gateway_method.http_methods.http_method, count.index)}"
+  status_code = "${element(aws_api_gateway_method_response.protectedresource_responses.status_code, count.index)}"
 }
 
 #  allow API Gateway to execute the Lambda functions
@@ -39,7 +67,7 @@ resource "aws_lambda_permission" "protectedresource_apigw_lambda_permission" {
 resource "aws_api_gateway_deployment" "protectedresource_prod" {
   rest_api_id = "${aws_api_gateway_rest_api.protectedresource.id}"
   stage_name  = "prod"
-  depends_on  = ["module.http_method"]
+  depends_on  = ["aws_api_gateway_method.http_methods", "aws_api_gateway_integration.protectedresource_integrations"]
 }
 
 # write the endpoint's invoke URL to S3, so it can be used by other APIs in the future
